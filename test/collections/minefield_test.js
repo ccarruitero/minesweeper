614fbe400e86be7d98c35f5874da63264b1b419e
Cesar Carruitero <cesar@mozilla.pe>
2013-07-31 14:20:16 -0500

    some tests

--- test/collections/minefield_test.js
+++ test/collections/minefield_test.js
@@ -1,32 +1,47 @@
 describe('Minefield', function(){
   var Minefield = new Application.Collections.minefield();
+  it('use defaults if not initial data', function(){
+    expect(Minefield.length).to.eql(80);
+    expect(Minefield.where({'hasMine': true}).length).to.eql(10);
+  });
+  it('use initial data instead of defaults', function(){
+    var NewMinefield = new Application.Collections.minefield(10,5,15);
+    console.log(NewMinefield.length);
+    expect(NewMinefield.length).to.eql(50);
+    expect(NewMinefield.where({'hasMine': true}).length).to.eql(15);
+  });
   describe('#getSize()', function(){
     it('respond with size of minefield', function(){
       expect(Minefield.getSize(5,3)).to.eql(15);
     });
   });
   describe('#addSquares', function(){
-    it('squares number must be equal size', function(){
-      var size = Minefield.getSize(10, 5);
-      Minefield.addSquares(size);
-      expect(Minefield.length).to.eql(50);
+    it('Minefield squares must be equal size', function(){
+      expect(Minefield.length).to.eql(Minefield.getSize(10,8));
     });
   });
   describe('#setMine', function(){
     it('should set mine', function(){
-      var otherMinefield = new Application.Collections.minefield();
+      Minefield.reset();
       var square = new Application.Models.square();
-      otherMinefield.add(square);
-      otherMinefield.setMine(square);
-      expect(otherMinefield.where({'hasMine': true}).length).to.eql(1);
+      Minefield.add(square);
+      Minefield.setMine(square);
+      expect(Minefield.where({'hasMine': true}).length).to.eql(1);
     });
   });
   describe('#addMines', function(){
     it('set mines in squares', function(){
       var mines = 10;
+      Minefield.reset();
       Minefield.addSquares(20);
       Minefield.addMines(mines);
       expect(Minefield.where({'hasMine': true}).length).to.eql(mines);
+    });
+  });
+  describe('#newGame', function(){
+    it('should initialize new game with same attributes', function(){
+      Minefield.newGame();
+      expect(Minefield.length).to.eql(80);
     });
   });
 });
